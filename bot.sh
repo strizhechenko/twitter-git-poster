41fa57c7a5043265b1f211ef427b5ae3d6630d23
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-11-09 16:09:24 +0600

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+. /etc/sysconfig/git_poster
 set -eu
+
 version="$1"
-bot_home=/opt/twitterbot_farm_bots/carbon_reductor/
+
 # shellcheck disable=SC2044
-for repo in $(find $bot_home/repos -maxdepth 1 -mindepth 1 -type d); do
+for repo in $(find $BOT_HOME/repos -maxdepth 1 -mindepth 1 -type d); do
 	$bot_home/__bot.sh "$repo" "$version"
 done
